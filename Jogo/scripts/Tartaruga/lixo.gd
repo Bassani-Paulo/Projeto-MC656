ece9357d554e173b4bac2bf96e864db68a497fe0
gustavoamaraal <g218669@dac.unicamp.br>
2024-11-26 00:38:04 -0300

    refactoring avaliacao4

--- Jogo/scripts/Tartaruga/lixo.gd
+++ Jogo/scripts/Tartaruga/lixo.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-@export var speed = 100 # Como rápido o canudo irá cair (pixels/seg).
+@export var speed = 100 # How fast the canudo will fall (pixels/sec).
 var canudo_height
 var canudo_width
 var visible_rect
 var scale_x
 var scale_y
 
-# Chamado quando o jogador entra na cena pela primeira vez.
+# Called when the player enters the scene for the first time.
 func _ready() -> void:
 	Global.spawn_rate += 1
 	get_parent().get_node("Timer").update_wait_time(self)
@@ -15,7 +15,7 @@ func _ready() -> void:
 	inicializar_dimensões()
 	set_posiçãoAleatoria()
 
-# Função para inicializar as dimensões do canudo
+# Function to initialize the dimensions of the straw
 func inicializar_dimensões() -> void:
 	scale_x = get_node("Sprite2D").scale.x
 	scale_y = get_node("Sprite2D").scale.y
@@ -25,19 +25,19 @@ func inicializar_dimensões() -> void:
 	print(canudo_width)
 	visible_rect = get_viewport().get_visible_rect()
 
-# Função para definir a posição aleatória do canudo
+# Function to set the straw's random position
 func set_posiçãoAleatoria() -> void:
 	position.x = randf_range(canudo_width / 2, visible_rect.size.x - canudo_width / 2)
 	position.y = -canudo_height
 
-# Chamado a cada quadro. 'delta' é o tempo decorrido desde o quadro anterior.
+# Called every frame. 'delta' is the time elapsed since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # Vetor de movimento do canudo.
 	
-	# Movimento do canudo
+	# Straw movement
 	velocity.y += speed
 	position += velocity * delta
 
-	# Se o canudo sair da tela, será removido
+	# If the straw goes off-screen, it will be removed
 	if position.y >= visible_rect.size.y + canudo_height / 2:
 		queue_free()
