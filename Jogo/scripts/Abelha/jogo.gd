09d857432a40611691968e7eed8135fecf89df92
Bassani-Paulo <bassanipaulo@gmail.com>
2024-12-02 23:36:26 -0300

    arrumando a velocidade dos sprites

--- Jogo/scripts/Abelha/jogo.gd
+++ Jogo/scripts/Abelha/jogo.gd
@@ -3,6 +3,7 @@ extends Node2D
 signal game_state_changed(new_speed)
 var game_state = 1
 const MAXSPEED = 3
+var speed_to_modifier = {0:0.75, 1:1, 2:1.25, 3:1.5}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,11 +17,13 @@ func accelerate():
 	if game_state<MAXSPEED:
 		game_state+=1
 		emit_signal("game_state_changed", game_state)
+		get_tree().call_group("object", "update_speed_modifier", speed_to_modifier[game_state])
 	
 func decelerate():
 	if game_state > 0:
 		game_state-=1
 		emit_signal("game_state_changed", game_state)
+		get_tree().call_group("object", "update_speed_modifier", speed_to_modifier[game_state])
 	else:
 		game_over()
 		
