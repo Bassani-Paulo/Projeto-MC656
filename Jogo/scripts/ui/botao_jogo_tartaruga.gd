f3701c139fd39b7276c2bf4712fab947811d926f
gustavoamaraal <g218669@dac.unicamp.br>
2024-12-03 00:53:10 -0300

    alteracoes menu

---
+++ Jogo/scripts/ui/botao_jogo_tartaruga.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+func _ready():
+	$TextureRect.hide()  # Garante que a imagem esteja oculta no início.
+
+func _on_mouse_entered():
+	$TextureRect.show()  # Mostra a imagem ao passar o mouse.
+
+func _on_mouse_exited():
+	$TextureRect.hide()  # Esconde a imagem ao sair do botão.
